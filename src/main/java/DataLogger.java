941650b8f61d74e598579ba266fae3f1316a7855
FTC 7244 <contact@brandonbarker.me>
2017-10-08 17:17:50 -0400

    Updated Launcher and Styling

--- src/main/java/DataLogger.java
+++ src/main/java/DataLogger.java
@@ -3,6 +3,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -10,7 +11,7 @@ import java.net.URL;
 /**
  * Created by OOTB on 11/28/2016.
  */
-public class PIDTuner extends Application {
+public class DataLogger extends Application {
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -19,6 +20,8 @@ public class PIDTuner extends Application {
     public void start(Stage stage) throws Exception {
         URL resource = getClass().getResource("/window.fxml");
         Parent root = FXMLLoader.load(resource);
+        stage.setTitle("Data Logger");
+        stage.getIcons().addAll(new Image("./icon.png"));
         stage.setScene(new Scene(root));
         stage.setOnCloseRequest(event -> Platform.exit());
         stage.show();
